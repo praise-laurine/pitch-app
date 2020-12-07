f566a23d0dad5fe69a9e6c04c6136f77672b9b97
praise-laurine <praiselaurine10@gmail.com>
2020-12-07 09:37:03 +0300

    imported SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+# from flask_simplemde import SimpleMDE
 from flask_mail import Mail
 
 
